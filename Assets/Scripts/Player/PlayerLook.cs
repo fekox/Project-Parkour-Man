1dbfccac287e0b982112a7af7667e50c28aa8e91
fekox <86255202+fekox@users.noreply.github.com>
2023-04-10 16:06:14 -0300

    Implement the old player movement and the first person camera

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLook : MonoBehaviour
+{
+    [Header("Setup")]
+
+    [SerializeField]
+    private Transform playerBody;
+
+    [Header("Movement")]
+
+    [SerializeField]
+    private float mouseSensitivity = 100f;
+
+    private Vector2 mouseRot;
+
+    private float xRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       var mouseX = mouseRot.x * mouseSensitivity * Time.deltaTime;
+       var mouseY = mouseRot.y * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+     
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    void OnLook(InputValue inputValue)
+    {
+        mouseRot = inputValue.Get<Vector2>();
+      
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const int MaxFloorDistance = 10;
+
+    [Header("Setup")][SerializeField] private Rigidbody rigidBody;
+
+    [SerializeField] private Transform feetPivot;
+
+    [Header("Movement")][SerializeField] private float movementSpeed = 10f;
+
+    [SerializeField] private float jumpForce = 10f;
+
+    [SerializeField] private float minJumpDistance = 0.25f;
+
+    [SerializeField] private float jumpBufferTime = 0.25f;
+
+    private Vector3 _currentMovement;
+    
+    private Coroutine _jumpCoroutine;
+
+    private bool _isJumpInput;
+
+    [SerializeField] private float coyoteTime;
+
+    private void OnValidate()
+    {
+        rigidBody ??= GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        if (!rigidBody)
+        {
+            enabled = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = _currentMovement * movementSpeed + Vector3.up * rigidBody.velocity.y;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        var movementInput = value.Get<Vector2>();
+        _currentMovement = new Vector3(movementInput.x, 0, movementInput.y);
+    }
+
+    public void OnJump()
+    {
+        if (_jumpCoroutine != null)
+            StopCoroutine(_jumpCoroutine);
+        _jumpCoroutine = StartCoroutine(JumpCoroutine());
+    }
+
+    private IEnumerator JumpCoroutine()
+    {
+        if (!feetPivot)
+            yield break;
+
+        for (var timeElapsed = 0.0f; timeElapsed <= jumpBufferTime; timeElapsed += Time.fixedDeltaTime)
+        {
+            yield return new WaitForFixedUpdate();
+            var isFalling = rigidBody.velocity.y <= 0;
+            var currentFeetPosition = feetPivot.position;
+
+            var canNormalJump = isFalling && CanJumpInPosition(currentFeetPosition);
+
+            var coyoteTimeFeetPosition = currentFeetPosition - rigidBody.velocity * coyoteTime;
+            var canCoyoteJump = isFalling && CanJumpInPosition(coyoteTimeFeetPosition);
+
+            if (!canNormalJump && canCoyoteJump)
+            {
+                Debug.DrawLine(currentFeetPosition, coyoteTimeFeetPosition, Color.green, 5f);
+            }
+
+            if (canNormalJump || canCoyoteJump)
+            {
+                var jumpForceVector = Vector3.up * jumpForce;
+
+                if (rigidBody.velocity.y < 0)
+                    jumpForceVector.y -= rigidBody.velocity.y;
+
+                rigidBody.AddForce(jumpForceVector, ForceMode.Impulse);
+
+                yield break;
+            }
+        }
+    }
+
+    private bool CanJumpInPosition(Vector3 currentFeetPosition)
+    {
+        return Physics.Raycast(currentFeetPosition, Vector3.down, out var hit, MaxFloorDistance)
+               && hit.distance <= minJumpDistance;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!feetPivot)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(feetPivot.position, feetPivot.position + Vector3.down * minJumpDistance);
+    }
+}
