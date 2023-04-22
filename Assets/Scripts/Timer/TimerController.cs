b2a4a91fd1abbae1bd726ebb51f27bd3444142fe
fekox <86255202+fekox@users.noreply.github.com>
2023-04-22 00:23:45 -0300

    Implement a timer in the UI

---
+++ Assets/Scripts/Timer/TimerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Text timerText;
+
+    private float timerSeg;
+
+    void Update()
+    {
+        timerSeg += Time.deltaTime;
+
+        timerText.text = string.Format("{00:00}", timerSeg);
+
+    }
+}
